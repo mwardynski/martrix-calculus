84a8be2e83a8b3b9eac50382aa0b7e07fda2eb29
mwardynski <m.wardynski@netconomy.net>
2024-03-09 15:55:10 +0100

    [prog1] read k range from cli

--- prog1/matrix_multiplication.js
+++ prog1/matrix_multiplication.js
@@ -1,1 +1,26 @@
-console.log("Matrix Multiplication")
+
+let kRange = []
+if (process.argv.length < 3) {
+    console.error('neither k nor k-range specified')
+    process.exit(1)
+} else {
+    kRange = readKRange(process.argv)
+}
+
+for (let k = kRange[0]; k <= kRange[1]; k++) {
+    mxSize = 2 ** k
+    console.log("k="+k+", matrix size="+mxSize)
+}
+
+function readKRange(args) {
+    let kRange = []
+    if (args.length == 4) {
+        let kLow = parseInt(args[2])
+        let kHigh = parseInt(args[3])
+        kRange = [kLow, kHigh]
+    } else {
+        let k = parseInt(args[2])
+        kRange = [k, k]
+    }
+    return kRange
+}
